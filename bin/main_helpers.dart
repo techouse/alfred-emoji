efdbb0cfc781dec4884b3b67a2947f6ca66c4f4b
Klemen Tušar <techouse@gmail.com>
2022-12-22 19:42:24 +0000

    :wrench: Disable Alfred smart result ordering in favor of Algolia ordering

--- bin/main_helpers.dart
+++ bin/main_helpers.dart
@@ -8,7 +8,7 @@ final AlfredWorkflow _workflow = AlfredWorkflow(
       Duration(days: 7),
     ),
   ),
-);
+)..disableAlfredSmartResultOrdering = true;
 
 final AlfredUpdater _updater = AlfredUpdater(
   githubRepositoryUrl: Uri.parse(Env.githubRepositoryUrl),
@@ -45,68 +45,60 @@ Future<void> _performSearch(
   );
 
   if (snapshot.nbHits > 0) {
-    final AlfredItems items = AlfredItems(
-      await Future.wait(snapshot.hits
-          .map((AlgoliaObjectSnapshot snapshot) =>
-              SearchResult.fromJson(snapshot.data))
-          .map((SearchResult emoji) async {
-        final File? image =
-            await EmojiDownloader(emoji: emoji.char).downloadImage();
+    _workflow.addItems(await Future.wait(snapshot.hits
+        .map((AlgoliaObjectSnapshot snapshot) =>
+            SearchResult.fromJson(snapshot.data))
+        .map((SearchResult emoji) async {
+      final File? image =
+          await EmojiDownloader(emoji: emoji.char).downloadImage();
 
-        return AlfredItem(
-          uid: emoji.objectID,
-          title: emoji.name,
-          subtitle: '${emoji.char} :${emoji.shortName}:',
-          arg: emoji.char,
-          match:
-              '${emoji.shortName} ${emoji.name} ${emoji.keywords.join(', ')}',
-          text: AlfredItemText(
-            copy: emoji.char,
-            largeType: emoji.name,
+      return AlfredItem(
+        uid: emoji.objectID,
+        title: emoji.name,
+        subtitle: '${emoji.char} :${emoji.shortName}:',
+        arg: emoji.char,
+        match: '${emoji.shortName} ${emoji.name} ${emoji.keywords.join(', ')}',
+        text: AlfredItemText(
+          copy: emoji.char,
+          largeType: emoji.name,
+        ),
+        quickLookUrl:
+            Uri.https('emojipedia.org', 'emoji/${emoji.char}/').toString(),
+        icon: AlfredItemIcon(path: image?.absolute.path ?? 'question.png'),
+        mods: {
+          {AlfredItemModKey.alt}: AlfredItemMod(
+            subtitle: 'Copy ":${emoji.shortName}:" to clipboard',
+            arg: ':${emoji.shortName}:',
+            icon: AlfredItemIcon(path: image?.absolute.path ?? 'question.png'),
           ),
-          quickLookUrl:
-              Uri.https('emojipedia.org', 'emoji/${emoji.char}/').toString(),
-          icon: AlfredItemIcon(path: image?.absolute.path ?? 'question.png'),
-          mods: {
-            {AlfredItemModKey.alt}: AlfredItemMod(
-              subtitle: 'Copy ":${emoji.shortName}:" to clipboard',
-              arg: ':${emoji.shortName}:',
-              icon:
-                  AlfredItemIcon(path: image?.absolute.path ?? 'question.png'),
-            ),
-            {AlfredItemModKey.shift}: AlfredItemMod(
-              subtitle: 'Copy Python source of "${emoji.char}" to clipboard',
-              arg: 'u"\\U000'
-                  '${emoji.char.runes.first.toRadixString(16).toUpperCase()}'
-                  '${emoji.char.runes.toList().sublist(1).map(
-                        (int i) => '\\u${i.toRadixString(16).toUpperCase()}',
-                      ).join()}"',
-              icon:
-                  AlfredItemIcon(path: image?.absolute.path ?? 'question.png'),
-            ),
-            {AlfredItemModKey.ctrl}: AlfredItemMod(
-              subtitle: 'Copy HTML Entity of "${emoji.char}" to clipboard',
-              arg: emoji.char.runes
-                  .map((int i) => '&#x${i.toRadixString(16)};')
-                  .join(),
-              icon:
-                  AlfredItemIcon(path: image?.absolute.path ?? 'question.png'),
-            ),
-            {AlfredItemModKey.ctrl, AlfredItemModKey.shift}: AlfredItemMod(
-              subtitle:
-                  'Copy formal Unicode notation of "${emoji.char}" to clipboard',
-              arg: emoji.char.runes
-                  .map((int i) => 'U+${i.toRadixString(16).toUpperCase()}')
-                  .join(', '),
-              icon:
-                  AlfredItemIcon(path: image?.absolute.path ?? 'question.png'),
-            ),
-          },
-          valid: true,
-        );
-      }).toList()),
-    );
-    _workflow.addItems(items.items);
+          {AlfredItemModKey.shift}: AlfredItemMod(
+            subtitle: 'Copy Python source of "${emoji.char}" to clipboard',
+            arg: 'u"\\U000'
+                '${emoji.char.runes.first.toRadixString(16).toUpperCase()}'
+                '${emoji.char.runes.toList().sublist(1).map(
+                      (int i) => '\\u${i.toRadixString(16).toUpperCase()}',
+                    ).join()}"',
+            icon: AlfredItemIcon(path: image?.absolute.path ?? 'question.png'),
+          ),
+          {AlfredItemModKey.ctrl}: AlfredItemMod(
+            subtitle: 'Copy HTML Entity of "${emoji.char}" to clipboard',
+            arg: emoji.char.runes
+                .map((int i) => '&#x${i.toRadixString(16)};')
+                .join(),
+            icon: AlfredItemIcon(path: image?.absolute.path ?? 'question.png'),
+          ),
+          {AlfredItemModKey.ctrl, AlfredItemModKey.shift}: AlfredItemMod(
+            subtitle:
+                'Copy formal Unicode notation of "${emoji.char}" to clipboard',
+            arg: emoji.char.runes
+                .map((int i) => 'U+${i.toRadixString(16).toUpperCase()}')
+                .join(', '),
+            icon: AlfredItemIcon(path: image?.absolute.path ?? 'question.png'),
+          ),
+        },
+        valid: true,
+      );
+    }).toList()));
   } else {
     _workflow.addItem(
       AlfredItem(
